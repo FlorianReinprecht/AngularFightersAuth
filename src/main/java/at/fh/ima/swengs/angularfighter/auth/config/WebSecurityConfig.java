a8bb7ebb4b423ad4f3c0891afd2ae0fa09e0b473
Florian Reinprecht <florian.reinprecht@edu.fh-joanneum.at>
2018-01-20 20:33:55 +0100

    Initial commit

---
+++ src/main/java/at/fh/ima/swengs/angularfighter/auth/config/WebSecurityConfig.java
@@ -0,0 +1,43 @@
+package at.fh.ima.swengs.angularfighter.auth.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void globalUserDetails(final AuthenticationManagerBuilder auth) throws Exception {
+        // @formatter:off
+	auth.inMemoryAuthentication()
+	  .withUser("john").password("123").roles("USER").and()
+	  .withUser("flo").password("111").roles("ADMIN");
+    }// @formatter:on
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        // @formatter:off
+		http.authorizeRequests().antMatchers("/login").permitAll()
+		.antMatchers("/oauth/token/revokeById/**").permitAll()
+		.antMatchers("/tokens/**").permitAll()
+		.anyRequest().authenticated()
+		.and().formLogin().permitAll()
+		.and().csrf().disable();
+		// @formatter:on
+    }
+
+}
